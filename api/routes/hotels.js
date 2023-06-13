4a5a9f885eede34782c624f17d885cba01be17f4
“Fadel <[fadelmuhammad128@gmail.com]>
2023-06-14 01:06:09 +0700

    first commit

---
+++ api/routes/hotels.js
@@ -0,0 +1,22 @@
+import express  from "express";
+import Hotel from "../models/Hotel.js";
+import {createError} from "../utils/error.js";
+import {createHotel, deleteHotel, getAllHotel, getHotel, updateHotel} from "../controllers/hotel.js";
+
+const router = express.Router();
+
+//CREATE
+router.post('/', createHotel);
+
+//UPDATE
+router.put('/:id', updateHotel);
+//DELETE
+router.delete('/:id',deleteHotel);
+//GET
+router.get('/:id', getHotel);
+//GET ALL
+router.get('/', getAllHotel);
+
+
+
+export default router
